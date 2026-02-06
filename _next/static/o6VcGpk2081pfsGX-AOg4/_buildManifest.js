6f1870f78605482087fc53fa943dc53f19c3a58f
Alexia Valenzuela <alexiav304@gmail.com>
2026-02-05 21:56:02 -0700

    Updates

---
+++ _next/static/o6VcGpk2081pfsGX-AOg4/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/anchor-fantasy-mode//_next/:path+",
+        "destination": "/anchor-fantasy-mode/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
