7b279237b37765523a8f363a8200b4ab04763f7b
Alexia Valenzuela <alexiav304@gmail.com>
2026-02-08 00:18:36 -0700

    deploying it to github pages

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,18 @@
 // next.config.ts
 import type { NextConfig } from "next";
 
-const isGithubPages = process.env.NODE_ENV === "production" && process.env.GITHUB_PAGES === "true";
-
 const repo = "anchor-fantasy-mode";
+const isGithubPages = process.env.GITHUB_PAGES === "true";
 
 const nextConfig: NextConfig = {
   output: "export",
-  images: { unoptimized: true },
+
   basePath: isGithubPages ? `/${repo}` : "",
-  assetPrefix: isGithubPages ? `https://alexiavalen.github.io/${repo}/` : "",
+  assetPrefix: isGithubPages ? `/${repo}/` : "",
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
